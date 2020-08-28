aeb768f3a3ae4a95c812a2b401d52a146636925e
mahdihawary <Mahdihawary@gmail.com>
2020-08-28 11:33:52 -0400

    completed pairlab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,43 @@
 class BankAccount
+    
+    attr_reader :name
+    attr_accessor :balance, :status
+    
+    @@all =[]
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status= "open"
+        @@all<<self
+    end
+
+    def deposit(amount)
+        self.balance += amount
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.status == "open" && self.balance > 0
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def transfers
+        Transfer.all.filter {|transfer| transfer.sender || transfer.receiver == self}
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  @@all=[]
+
+  def self.all
+    @@all
+  end
+
+  def initialize(sender, receiver, amount)
+    @receiver=receiver
+    @sender=sender
+    @status = "pending"
+    @@all<<self
+    @amount = amount
+  end
+  def valid?
+    # if BankAccount.all.find{|account| account.name == sender}.valid? && BankAccount.all.find{|account| account.name == receiver}.valid?
+    #   true
+    # else 
+    #   false
+    # end
+    if sender.valid? && receiver.valid?
+      true
+    else 
+      false
+    end
+  end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status != "complete"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
 end
